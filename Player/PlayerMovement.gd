4ac443bf5688a40b335d42114f659b7981af032f
deepdass <deepdas26052008@gmail.com>
2026-07-07 21:23:11 +0530

    Water Test

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -22,7 +22,13 @@ func _ready() -> void:
 		
 		
 
+func _process(delta: float) -> void:
+	RenderingServer.global_shader_parameter_set("player_position", global_position)
+	print("player_position sent: ", global_position)
+
 func _physics_process(delta: float) -> void:
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
