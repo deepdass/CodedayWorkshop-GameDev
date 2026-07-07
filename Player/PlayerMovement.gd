b324c492c15bf72e0dae185d84e36fbbc79d9860
deepdass <deepdas26052008@gmail.com>
2026-07-07 19:16:09 +0530

    mesh switch

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -5,7 +5,22 @@ const SPEED = 7.0
 const JUMP_VELOCITY = 3.0
 
 @onready var visuals: Node3D = $Visuals
-@onready var animation_player: AnimationPlayer = $Visuals/Car/catbanana/AnimationPlayer
+
+@export var IsMage : bool = false
+@onready var car: Node3D = $Visuals/Car
+@onready var mage: Node3D = $Visuals/Mage
+@onready var animation_player: AnimationPlayer = $Visuals/Mage/AnimationPlayer
+@onready var animation_player_car: AnimationPlayer = $Visuals/Car/catbanana/AnimationPlayer
+
+func _ready() -> void:
+	if IsMage:
+		mage.visible = true
+		car.visible = false
+	else:
+		car.visible = true
+		mage.visible = false
+		
+		
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -25,13 +40,20 @@ func _physics_process(delta: float) -> void:
 		velocity.z = direction.z * SPEED
 		
 		var target_rotation = atan2(direction.x, direction.z)
-		visuals.rotation.y = lerp_angle(visuals.rotation.y, target_rotation, 6.0 * delta)
+		visuals.rotation.y = lerp_angle(visuals.rotation.y, target_rotation, 8.0 * delta)
 		
-		animation_player.play("metarigAction_001")
+		if IsMage:
+			animation_player.play("Walking_A")
+		else:
+			animation_player_car.play("metarigAction_001")
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		animation_player.seek(0)
-
+		
+		if IsMage:
+			animation_player.play("Idle")
+		else:
+			animation_player_car.seek(0)
+		
 	move_and_slide()
